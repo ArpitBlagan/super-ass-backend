66fb5438dec5d2d7b1ce79af2d7fd4e3d2172e25
arpit <blaganarpit@gmail.com>
2023-12-17 13:54:54 +0530

    done

--- Router.js
+++ Router.js
@@ -9,11 +9,11 @@ Router.route("/all").get(getAll)
 Router.post("/userRes",upload.none(),async(req,res)=>{
     const{name,form_id,cat,comp,cloze}=req.body;
     console.log(req.body);
-    // const data=await uDB.create({form_id,name,category:JSON.parse(cat),cloze:JSON.parse(cloze),
-    //     comprehension:JSON.parse(comp)});
-    // if(data){
-    // return res.status(200).json({message:"Created"});
-    // }
-    // res.status(400).json({message:"something went wrong"})
+    const data=await uDB.create({form_id,name,category:JSON.parse(cat),cloze:JSON.parse(cloze),
+        comprehension:JSON.parse(comp)});
+    if(data){
+    return res.status(200).json({message:"Created"});
+    }
+    res.status(400).json({message:"something went wrong"})
 });
 module.exports=Router
